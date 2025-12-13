7b24a76833703927044e461d6e9392017b7de143
Desolateeeooo <125212823+Desolateeeooo@users.noreply.github.com>
2025-12-13 18:46:07 +0500

    Update App component
    Add routing + add HomePage.tsx, SignInPage.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,15 @@
-import styles from './App.module.css';
-import { Footer, Header } from './components/layout';
-import { GuidesSection, SearchBar, SubscriptionCard, TopicsGrid } from './components/features/home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
-  return (
-    <div className={styles.container}>
-			<Header />
-      {/* ===== MAIN CONTENT ===== */}
-      <main className={styles.main}>
-        <div className={styles.mainContent}>
-          {/* Hero Section */}
-          <section className={styles.hero}>
-            <h1 className={styles.heroTitle}>
-              English-Russian Dictionary for On-Demand IT Vocabulary
-            </h1>
-						<SearchBar/>
-            <p className={styles.heroSubtitle}>
-              The brand new English-Russian dictionary for <span>beginner to advanced levels</span> proficiency
-            </p>
-          </section>
-					<SubscriptionCard />
-					<TopicsGrid />
-					<GuidesSection />
-        </div>
-      </main>
-			<Footer />
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				{/*<Route path="/signin" element={<SignInPage />} />*/}
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
