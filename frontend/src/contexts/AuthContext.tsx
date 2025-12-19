f12d56b341e073fde12771126bfb21aabaed6a80
Desolateeeooo <125212823+Desolateeeooo@users.noreply.github.com>
2025-12-19 20:32:39 +0500

    Add AuthContext + useAuth custom hook

---
+++ frontend/src/contexts/AuthContext.tsx
@@ -0,0 +1,71 @@
+import axios from 'axios';
+import { createContext, useEffect, useState, type ReactNode } from 'react';
+
+interface User {
+  id: number;
+  username: string;
+  email: string;
+}
+
+interface AuthContextType {
+  user: User | null;
+  loading: boolean;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+  checkAuth: () => Promise<void>;
+}
+
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+axios.defaults.withCredentials = true;
+
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  const checkAuth = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/auth/me');
+      setUser(response.data.user);
+    } catch (error) {
+      setUser(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const login = async (email: string, password: string) => {
+    try {
+      const response = await axios.post('http://localhost:5000/api/auth/login', {
+        email,
+        password,
+      });
+
+      setUser(response.data.user);
+    } catch (error: any) {
+      if (axios.isAxiosError(error) && error.response) {
+        throw new Error(error.response.data.error || 'Login failed.');
+      }
+      throw new Error('Network error.');
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await axios.post('http://localhost:5000/api/auth/logout');
+      setUser(null);
+    } catch (error) {
+      console.log('Logout error:', error);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, loading, login, logout, checkAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
