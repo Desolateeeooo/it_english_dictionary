a08698f20478ee8b1ccc46ef49d64ae724ba99d2
Desolateeeooo <125212823+Desolateeeooo@users.noreply.github.com>
2026-01-04 23:16:12 +0500

    Add bcryptjs to the backend dependencies
    - Update package.json
    - Create helper functions to hash passwords

---
+++ backend/src/data/passwordHelperFuncs.ts
@@ -0,0 +1,23 @@
+import bcrypt from 'bcryptjs';
+
+export const passwordHash = async (password: string, saltRounds: number) => {
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    return await bcrypt.hash(password, salt);
+  } catch (err) {
+    console.log(err);
+  }
+  return null;
+};
+
+export const comparePasswords = async (password: string, hash: string) => {
+  try {
+    const matchFound = await bcrypt.compare(password, hash);
+    return matchFound;
+  } catch (err) {
+    console.log(err);
+  }
+  return false;
+};
+
+
